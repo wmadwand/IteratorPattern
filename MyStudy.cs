fcc1ee8841391c7977183dd9c612f820ca6b4563
wmadwand <wmadwand@gmail.com>
2018-10-06 13:11:11 +0300

    IteratorFactory class. One more concrete iterator with reverse order.

--- MyStudy.cs
+++ MyStudy.cs
@@ -26,7 +26,7 @@ namespace IteratorMS01
     // aggregate
     public interface IBookCollection
     {
-        IBookIterator CreateIterator();
+        IBookIterator CreateIterator(IIteratorFactory factory, BookIteratorType type);
         int Count { get; }
         Book this[int index] { get; set; }
     }
@@ -59,14 +59,75 @@ namespace IteratorMS01
         public Book CurrentItem { get { return _collection[_current]; } }
     }
 
+    public class LibraryReverseIterator : IBookIterator
+    {
+        private IBookCollection _collection;
+        private int _current;
+
+        public LibraryReverseIterator(IBookCollection collection)
+        {
+            _collection = collection;
+        }
+
+        public bool IsDone { get { return _current < 0; } }
+
+        public Book CurrentItem { get { return _collection[_current]; } }
+
+        public Book First()
+        {
+            _current = _collection.Count - 1;
+            return _collection[_current];
+        }
+
+        public Book Next()
+        {
+            _current--;
+            return !IsDone ? _collection[_current] : null;
+        }
+    }
+
+    public enum BookIteratorType
+    {
+        normal,
+        reverse
+    }
+
+    public interface IIteratorFactory
+    {
+        IBookIterator Create(IBookCollection collection, BookIteratorType type);
+    }
+
+    public class IteratorFactory : IIteratorFactory
+    {
+        public virtual IBookIterator Create(IBookCollection collection, BookIteratorType type)
+        {
+            IBookIterator iterator = null;
+
+            switch (type)
+            {
+                case BookIteratorType.normal:
+                    iterator = new LibraryIterator(collection);
+                    break;
+                case BookIteratorType.reverse:
+                    iterator = new LibraryReverseIterator(collection);
+                    break;
+                default:
+                    iterator = new LibraryIterator(collection);
+                    break;
+            }
+
+            return iterator;
+        }
+    }
+
     // concrete aggregate
     public class Library : IBookCollection
     {
         private ArrayList _items = new ArrayList();
 
-        public IBookIterator CreateIterator()
+        public IBookIterator CreateIterator(IIteratorFactory factory, BookIteratorType type)
         {
-            return new LibraryIterator(this);
+            return factory.Create(this, type);
         }
 
         public int Count { get { return _items.Count; } }
@@ -80,10 +141,8 @@ namespace IteratorMS01
 
     public class Reader
     {
-        public void SeeBooks(Library library)
+        public void SeeBooks(IBookCollection library, IBookIterator iterator)
         {
-            IBookIterator iterator = library.CreateIterator();
-
             for (Book item = iterator.First(); !iterator.IsDone; item = iterator.Next())
             {
                 Console.WriteLine(iterator.CurrentItem.Name);
@@ -91,18 +150,22 @@ namespace IteratorMS01
         }
     }
 
-
     class Program
     {
         static void Main(string[] args)
         {
-            Library library = new Library();
+            IBookCollection library = new Library();
+            IIteratorFactory iteratorFactory = new IteratorFactory();
+
+            IBookIterator iteratorNormal = library.CreateIterator(iteratorFactory, BookIteratorType.normal);
+            IBookIterator iteratorReverse = library.CreateIterator(iteratorFactory, BookIteratorType.reverse);
 
             library[0] = new Book("book01");
             library[1] = new Book("book02");
 
             Reader reader = new Reader();
-            reader.SeeBooks(library);
+            reader.SeeBooks(library, iteratorNormal);
+            reader.SeeBooks(library, iteratorReverse);
 
             Console.ReadKey();
         }
